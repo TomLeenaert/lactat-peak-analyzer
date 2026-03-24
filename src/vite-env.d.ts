c130cc19c35ae5a788aecc1489e10b94ca425542
TomLeenaert <tomleenaert@gmail.com>
2026-03-24 10:25:26 +0100

    feat: add build stamp type declarations

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,6 @@
 /// <reference types="vite/client" />
 
 declare const __APP_VERSION__: string;
-
+declare const __BUILD_DATE__: string;
+declare const __BUILD_TIME__: string;
+declare const __GIT_BRANCH__: string;
